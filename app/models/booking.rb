80958bb83d858db79c91c607ced5ff2440eec8ae
Chinar Regundwar <chinarregundwar@gmail.com>
2018-02-28 07:07:52 +0000

    calculating and displaying total price for a ticket

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -4,6 +4,7 @@ class Booking < ApplicationRecord
   has_many :booking_seats
   has_many :seats, through: :booking_seats
   attr_accessor :no_of_tickets
+  before_save :set_total_price
   validates_presence_of :no_of_tickets
   validates :no_of_tickets, numericality: {greater_than: 0}
   validate :available_tickets
@@ -14,5 +15,9 @@ class Booking < ApplicationRecord
     unless self.showing.available_seats(self.booking_date).count >= self.no_of_tickets.to_i
       errors.add(:base, "Seats are not available.")
     end
+  end
+  
+  def set_total_price
+    self.total_price = self.no_of_tickets.to_i * self.showing.ticket_price
   end
 end
