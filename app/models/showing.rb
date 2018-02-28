934eb906a7c08e9ed350dec4c01eb52e513743c4
Chinar Regundwar <chinarregundwar@gmail.com>
2018-02-28 10:25:12 +0530

    calculatng available seats for a show

--- app/models/showing.rb
+++ app/models/showing.rb
@@ -5,6 +5,6 @@ class Showing < ApplicationRecord
   enum show_timing: ["12:00 PM", "3:00 PM", "6:00 PM", "9:00 PM"]
 
   def available_seats(movie_date)
-    self.cinema_hall.seats - (self.cinema_hall.seats.joins(:booking_seats).where(:'booking_seats.booking_date' => movie_date))
+    self.cinema_hall.seats - (self.cinema_hall.seats.joins(:booking_seats).where(:'booking_seats.booking_date' => movie_date, :'booking_seats.showing_id' => self.id))
   end
 end
